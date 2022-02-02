ada9e96e2b8e7fa60a0e8dea992d5040c751d660
Ahmad Macbook Air <ahmaddin211056@gmail.com>
2022-02-02 22:20:27 +0700

    basic configuration

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const connection = require('./app/config/database');
+const router = require('./app/router');
+
+connection();
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+app.use('/',router);
+
+app.listen(port, () => {
+    console.log(`App listening on port http://localhost:${port}`)
+})
