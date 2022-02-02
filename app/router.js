8c846eb450d23c3b2fa24d11f41cb8abcc619183
Ahmad Macbook Air <ahmaddin211056@gmail.com>
2022-02-02 22:35:23 +0700

    add login routes

--- app/router.js
+++ app/router.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const userRoute = require('./controllers/users')
+const userRoute = require('./controllers/users');
+const loginRoute = require('./controllers/login');
 
 router.get('/',async (req,res)=>{
     res.send('API Worked')
 })
 
-router.get('/',async (req,res)=>{
-    res.send('API Worked')
-})
+router.use('/login',loginRoute);
 
 router.use('/user',userRoute);
 
