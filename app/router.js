00f69887f169690f8bcf640ffa6494303cabb016
Ahmad Macbook Air <ahmaddin211056@gmail.com>
2022-02-04 22:46:47 +0700

    add items base route

--- app/router.js
+++ app/router.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 const userRoute = require('./controllers/users');
 const loginRoute = require('./controllers/login');
+const itemsRoutes = require('./controllers/items');
 
 router.get('/',async (req,res)=>{
     res.send('API worked well')
 })
 
 router.use('/login',loginRoute);
-
 router.use('/user',userRoute);
+router.use('/items',itemsRoutes);
 
 module.exports = router;
